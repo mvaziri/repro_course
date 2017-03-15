e63eb2620704b9193cc920e676f0709539e561e3
mvaziri <mvaziri.p@gmail.com>
2017-03-15 11:28:33 -0400

    move metasearch into untracked directory

--- metasearch_analysis.py
+++ metasearch_analysis.py
@@ -1,2 +1,4 @@
 # coding: utf-8
 get_ipython().system('git clone https://github.com/OpenNeuroLab/metasearch.git)
+Path('data_not_in_repo').mkdir()
+get_ipython().magic('mv metasearch data_not_in_repo/')
